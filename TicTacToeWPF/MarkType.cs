35aa1df9448c2e6a45f75a95cee8264363371f28
Morten <morto1995@hotmail.com>
2018-10-30 16:01:30 +0100

    Done

---
+++ TicTacToeWPF/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToeWPF
+{
+    /// <summary>
+    /// The type of value a cell in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is X
+        /// </summary>
+        Cross
+    }
+}
